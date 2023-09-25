2f8143368dbd34ff9aea45733d4114f964b91028
Computo 3 Zacatecoluca <zc201@itca-fepade.edu.sv>
2023-09-25 14:14:35 -0600

    ya la cagas yordi

--- app/Http/Controllers/PrestamosController.php
+++ app/Http/Controllers/PrestamosController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\prestamos;
 use Illuminate\Http\Request;
 
 class PrestamosController extends Controller
 {
-    //
+    public function index()
+    {
+        // Renderiza la vista del formulario
+        return view('prestamos.registrar');
+    }
+
+    public function store(Request $request)
+    {
+        // Valida los datos del formulario
+        $this->validate($request, [
+            'nombres' => 'required',
+            'apellidos' => 'required',
+            'email' => 'required|email',
+            'telefono' => 'required',
+            'direccion' => 'required',
+            'fecha' => 'required',
+        ]);
+
+        // Guarda los datos en la base de datos
+        $prestamo = new prestamos();
+        $prestamo->nombres = $request->input('nombres');
+        $prestamo->apellidos = $request->input('apellidos');
+        $prestamo->email = $request->input('email');
+        $prestamo->telefono = $request->input('telefono');
+        $prestamo->direccion = $request->input('direccion');
+        $prestamo->fecha = $request->input('fecha');
+        $prestamo->save();
+
+        // Redirige al usuario a la página de listado de préstamos
+        return redirect()->route('prestamos.index')->with('message', 'El préstamo se ha registrado correctamente.');
+    }
 }
---
+++ resources/views/prestamos_libros.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Formulario de préstamo</title>
+</head>
+<body>
+  <h1>Formulario de préstamo</h1>
+
+  <form action="enviar-prestamo.php" method="post">
+    <label for="nombres">Nombres:</label>
+    <input type="text" name="nombres" id="nombres" required>
+
+    <label for="apellidos">Apellidos:</label>
+    <input type="text" name="apellidos" id="apellidos" required>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" required>
+
+    <label for="telefono">Teléfono:</label>
+    <input type="tel" name="telefono" id="telefono" required>
+
+    <label for="direccion">Dirección:</label>
+    <input type="text" name="direccion" id="direccion" required>
+
+    <label for="fecha">Fecha del préstamo:</label>
+    <input type="date" name="fecha" id="fecha" required>
+
+    <input type="submit" value="Enviar">
+  </form>
+</body>
+</html>
